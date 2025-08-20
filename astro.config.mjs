c34b20c0d9bf35d5d7e87ef803c71747427ac25c
Aetherayel <zcheilcz@gmail.com>
2025-08-19 22:06:50 -0500

    feat: update configuration and add new content for worldview essays

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,20 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import solidJs from "@astrojs/solid-js"
+import { fileURLToPath } from "node:url"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-sphere-demo.vercel.app",
   integrations: [mdx(), sitemap(), solidJs(), tailwind({ applyBaseStyles: false })],
+    vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@components": fileURLToPath(new URL("./src/components", import.meta.url)),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+      },
+    },
+  },
 })
